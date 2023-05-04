21b5400bff5873fcb23cd4a0edf38b367c056add
Bahashwan <bahashwan@mail.ru>
2023-05-04 17:12:30 +0300

    babelRc Added, Layout-NavBar-MainPage and routes Created

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+const React = require('react');
+const Navbar = require('./NavBar.jsx');
+
+module.exports = function Layout({ title, children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title}</title>
+
+<link rel="stylesheet" href="/styles/style.css" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+        ></link>
+      </head>
+      <body>
+        <Navbar />
+
+        {children}
+        </body>
+    </html>
+  );
+};
