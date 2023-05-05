1a49b445294cf7c4c0310b5ad1d3a8354bfdd7d4
Bahashwan <bahashwan@mail.ru>
2023-05-05 11:07:39 +0300

    PlayerName Display DONE!

--- components/Play.jsx
+++ components/Play.jsx
@@ -1,9 +1,10 @@
 const React = require('react');
 const CardDecComponent = require('./CardDecComponent');
 const Layout = require('./Layout');
-module.exports = function Play({ title, cardDecdb }) {
+module.exports = function Play({ title, cardDecdb ,name}) {
   return (
     <Layout title={title}>
+      <div className='name'>Hello, {name}!</div>
       <div className="PlayDiv card">
         {cardDecdb.map((cardDecs) => {
           return <CardDecComponent cardDecs={cardDecs} ></CardDecComponent>;
--- routes/play.routes.js
+++ routes/play.routes.js
@@ -4,13 +4,13 @@ const Play = require('../components/Play');
 
 router.get('/play', async (req, res) => {
   try {
-    const cardDecdb = await cardDec.findAll({raw: true});
+    const cardDecdb = await cardDec.findAll({ raw: true });
     // console.log(cardDecdb, '------------------')
-    res.status(200).renderComponent(Play, { title: 'Play FlashCards', cardDecdb });
+    const name = res.app.locals.name;
+    res.status(200).renderComponent(Play, { title: 'Play FlashCards', cardDecdb, name });
   } catch (error) {
     console.log(error.message);
   }
 });
 
-
-module.exports = router
+module.exports = router;
